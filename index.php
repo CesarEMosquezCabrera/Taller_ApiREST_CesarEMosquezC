4cf7c7cc05f9f2cd54dd1384e52c9a0947aedb5d
CesarEMosquezCabrera <cmeulogio24@gmail.com>
2024-09-26 11:37:16 -0400

    taller APIREST clase 3 "Conexión a base de datos y manejo de rutas"

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 //conntroladores
 require_once "controlador/rutas.controlador.php";
+require_once "controlador/cursos.controlador.php";
+
+//modelos
+require_once "modelo/cursos.modelo.php";
+
 
 $rutas=new ControladorRutas();
 $rutas->inicio();
--- rutas/rutas.php
+++ rutas/rutas.php
@@ -1,6 +1,24 @@
 <?php
 
 $array=explode("/",$_SERVER["REQUEST_URI"]);
-var_dump($array);
+//var_dump($array);
+
+if(count(array_filter($array))==1){
+    $json=array(
+        "detalle"=>"Sin solicitudes",
+    );
+    echo json_encode($json,true);
+}else{
+    if(count(array_filter($array))==4){
+
+        if(array_filter($array)[4]=="cursos"){
+            if(isset($_SERVER["REQUEST_METHOD"]) && $_SERVER["REQUEST_METHOD"]=="GET"){
+                $cursos=new ControladorCursos();
+                $cursos->index();
+            }
+        }
+
+    }
+}
 
 ?>
