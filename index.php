631f074b963954c38be91ac8a7534b9a6a5b7683
CesarEMosquezCabrera <cmeulogio24@gmail.com>
2024-10-01 11:23:39 -0400

    4 taller APIREST clase 4 "Validación de Acceso a recursos"

--- index.php
+++ index.php
@@ -2,9 +2,11 @@
 //conntroladores
 require_once "controlador/rutas.controlador.php";
 require_once "controlador/cursos.controlador.php";
+require_once "controlador/clientes.controlador.php";
 
 //modelos
 require_once "modelo/cursos.modelo.php";
+require_once "modelo/clientes.modelo.php";
 
 
 $rutas=new ControladorRutas();
--- rutas/rutas.php
+++ rutas/rutas.php
@@ -9,9 +9,13 @@ if(count(array_filter($array))==1){
     );
     echo json_encode($json,true);
 }else{
+    //utilizo el 4 por que tengo el archivo en otro lado
     if(count(array_filter($array))==4){
 
         if(array_filter($array)[4]=="cursos"){
+
+
+            //ver lista de cursos
             if(isset($_SERVER["REQUEST_METHOD"]) && $_SERVER["REQUEST_METHOD"]=="GET"){
                 $cursos=new ControladorCursos();
                 $cursos->index();
